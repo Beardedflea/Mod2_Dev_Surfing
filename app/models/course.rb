75d1a76fb7f2f190f943e93fec4b3b8ec2be2518
github username <github email address>
2019-01-24 11:29:34 +0000

    Added more course attributes, a Day model/class, and general touch ups

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,10 +5,21 @@ class Course < ApplicationRecord
     has_many :attendances
     has_many :students, through: :attendances, class_name: "User"
 
-    # validates :first_name 
+    validates :name, :description, presence: true
+    validates :name, uniqueness: true
+    validates :name, length: { in: 5..200}
+    validates :description, length: { in: 10..1500}
+    validates :start_date, :end_date, presence: true
+    #validates :start_date, numericality: {less_than_or_equal_to: Date.today}
+    #validates :end_date, numericality: { :greater_than start_date}
 
 
     def date_list
        return self.start_date.strftime("%d/%m/%Y") + " to " + self.end_date.strftime("%d/%m/%Y")
     end
+
+    def lesson_time
+        self.lesson_duration.strftime("%l:%M")
+    end
+
 end 
